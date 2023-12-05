1878b884d94fc88ee9256a43297ceacc9018467b
Alexander Kozic <akozic@bowdoin.edu>
2023-12-05 14:56:10 -0500

    [Implementation] Completed chord production and switching between two sections

--- music_generation.rb
+++ music_generation.rb
@@ -1,36 +1,65 @@
-# Welcome to Sonic Pi
-puts scale(:c4, :major)
-
-puts chord_degree(:iii, :c4, :major)
-
-##| progression = (ring, :i, :v, :vi, :iv)
-
-
+# Setup code
+use_random_seed Time.now.to_i
+roots = [:c4, :d4, :e4, :f4, :g4, :a4, :b4]
+type = [:major, :minor]
 chord_numerals = [:i, :ii, :iii, :iv, :v, :vi, :vii]
 
-# A section choosing
+
+# key choosing
+key = [roots.choose, type.choose]
+
+# Chord section choosing
 size=3
 a_chords=[]
+b_chords=[]
 for i in 0..size
-  a_chords[i]=choose(chord_numerals)
+  a_chords[i]= chord_numerals.choose
+  b_chords[i]= chord_numerals.choose
+end
+progression_a = a_chords.ring
+progression_b = b_chords.ring
+
+# bpm selection
+bpm = 60 + rand(60)
+use_bpm bpm
+
+# melody rythm selection
+rythm_length_a = rand(10)
+a_rythm = []
+a_notes = []
+a_scale = scale key[0], key[1], num_octaves: 2
+for i in 0...rythm_length_a
+  a_rythm[i] = 0.3 + rand(0...3)
+  a_notes[i] = a_scale.choose
 end
 
-print a_chords
 
-progression = a_chords.ring
-
-# B section choosing
-
-
-
-use_bpm 90
-
-live_loop :prog do
-  play chord_degree(progression.tick, :c3, :major, 3), release: 4
-  sleep 4
+# play song
+live_loop :song do
+  
+  in_thread do
+    8.times do
+      play chord_degree(progression_a.tick, key[0], key[1], 3), release: 4
+      sleep 4
+    end
+  end
+  
+  loop do
+    play_pattern_timed a_notes, a_rythm
+  end
+  
+  puts "enterting section b"
+  
+  in_thread do
+    8.times do
+      play chord_degree(progression_b.tick, key[0], key[1], 3), release: 4
+      sleep 4
+    end
+  end
+  
 end
 
-##| live_loop :melody do
+##| live_loop :melody_a  do
 ##|   play scale(:c4, :major, num_octaves: 2).choose
 ##|   sleep 0.5
 ##| end
