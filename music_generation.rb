66490542cfdd938212e9013d339e8c79ea75b802
Alexander Kozic <akozic@bowdoin.edu>
2023-12-06 22:50:21 -0500

    [Implementation] Completed music generation

--- music_generation.rb
+++ music_generation.rb
@@ -20,7 +20,7 @@ progression_a = a_chords.ring
 progression_b = b_chords.ring
 
 # bpm selection
-bpm = 60 + rand(60)
+bpm = 30 + rand(30)
 use_bpm bpm
 
 # melody rythm selection
@@ -33,33 +33,48 @@ for i in 0...rythm_length_a
   a_notes[i] = a_scale.choose
 end
 
+use_random_seed Time.now.to_i
 
-# play song
-live_loop :song do
-  
-  in_thread do
-    8.times do
-      play chord_degree(progression_a.tick, key[0], key[1], 3), release: 4
-      sleep 4
-    end
-  end
-  
-  loop do
-    play_pattern_timed a_notes, a_rythm
-  end
-  
-  puts "enterting section b"
-  
-  in_thread do
-    8.times do
-      play chord_degree(progression_b.tick, key[0], key[1], 3), release: 4
-      sleep 4
-    end
-  end
-  
+rythm_length_b = rand(10)
+b_rythm = []
+b_notes = []
+b_scale = scale key[0], key[1], num_octaves: 2
+for i in 0...rythm_length_b
+  b_rythm[i] = 0.3 + rand(0...5)
+  b_notes[i] = b_scale.choose
 end
 
-##| live_loop :melody_a  do
-##|   play scale(:c4, :major, num_octaves: 2).choose
-##|   sleep 0.5
-##| end
+# Play song
+in_thread do
+  use_synth :hoover
+  loop do
+    8.times do
+      play chord_degree(progression_a.tick, key[0], key[1], 3), amp: 0.3, release: 5
+      sleep 4
+    end
+    8.times do
+      play chord_degree(progression_b.tick, key[0], key[1], 4), amp: 0.3, release: 5
+      sleep 4
+    end
+  end
+end
+
+melody_switcher = 0
+b_mel_switcher = rand(1..3).to_i
+
+use_synth :hollow
+loop do
+  if melody_switcher == b_mel_switcher
+    melody_switcher = 0
+    puts "b"
+    4.times do
+      play_pattern_timed b_notes, b_rythm
+    end
+  else
+    puts "a"
+    3.times do
+      play_pattern_timed a_notes, a_rythm
+    end
+    melody_switcher += 1
+  end
+end
